eec935011e376e647b5fd8115cade65a2fec2277
Paige <paigeberliner@gmail.com>
2024-06-29 14:33:03 -0400

    Complete Lab

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,8 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
-from Course import Course
+from Course import Course 
+import json
 import ipdb
 
-
 class Scraper:
-    pass
+    def __init__(self):
+        self.courses = []
+
+    def get_page(self):
+        doc =  BeautifulSoup(requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text, 'html.parser')
+
+        for course in doc.select('.post'):
+            print(type(course))
+
+            title = course.select("h2")[0].text if course.select("h2") else ''
+            date = course.select(".date")[0].text if course.select(".date") else ''
+            description = course.select("p")[0].text  if course.select("p") else ''
+
+            new_course = Course(title, date, description)
+            self.courses.append(new_course)
+
